07353d54bf8ab38b52a888032a6e36935cf1b153
Laura Roncallo <lrlafontt@gmail.com>
2021-09-20 15:15:27 +0100

    solved

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ProfileImage({image}){
+    return <img src={image} className="profile" alt="profile"/>
+}
+export default ProfileImage;
+
+// "https://i.imgur.com/9yw1Fyw.jpg
